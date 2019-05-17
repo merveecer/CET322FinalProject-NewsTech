1390e919161150135f67b98e8a37620c7633969b
Merve Ecer <mervee.ecer@gmail.com>
2019-05-17 12:52:01 +0300

    Employee Create Formu oluşturuldu. Ve Layoutta ufak değişiklikler yapıldı.

--- NewsTech/Controllers/EmployeeController.cs
+++ NewsTech/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NewsTech.Data;
 using NewsTech.Models;
 
@@ -19,6 +20,23 @@ namespace NewsTech.Controllers
 			_roleManager = roleManager;
 			_userManager = userManager;
 		}
+
+		private IList<SelectListItem> GetAvailableRoles(IList<IdentityRole> roles) {
+			var availableRoles = new List<SelectListItem>();
+			foreach (var role in roles) {
+				availableRoles.Add(new SelectListItem {
+					Value = role.Id.ToString(),
+					Text = role.Name
+				});
+
+			}
+			availableRoles.Insert(0, new SelectListItem {
+				Value = "0",
+				Text = "Please select a role"
+			});
+			return availableRoles;
+		}
+
 		public IActionResult List() {
 			var employees = _context.Users.Where(x => x.isEmployee).ToList();
 			var employeesmodel = new List<EmployeeModel>();
@@ -35,5 +53,28 @@ namespace NewsTech.Controllers
 
 			return View(employeesmodel);
 		}
+
+		public IActionResult Create() {
+			EmployeeModel employeeModel = new EmployeeModel();
+			var roles = _context.Roles.OrderBy(x => x.Name).ToList();
+			employeeModel.AvailableRoles = GetAvailableRoles(roles);
+			//It is taken from Stackoverflow https://stackoverflow.com/questions/20789968/mvc4-dropdown-menu-for-gender
+			IEnumerable<GenderType> GenderType = Enum.GetValues(typeof(GenderType)) .Cast<GenderType>();
+			IEnumerable<MaritalStatus> MaritalStatus = Enum.GetValues(typeof(MaritalStatus)).Cast<MaritalStatus>();
+
+			employeeModel.GenderList= from gender in GenderType
+									  select new SelectListItem {
+										  Text = gender.ToString(),
+										  Value = ((int)gender).ToString()
+									  };
+			employeeModel.MaritalStatus = from maritalStatus in MaritalStatus
+										  select new SelectListItem {
+										   Text = maritalStatus.ToString(),
+										   Value = ((int)maritalStatus).ToString()
+									   };
+			return View(employeeModel);
+			
+		}
+
 	}
 }
--- NewsTech/Models/EmployeeModel.cs
+++ NewsTech/Models/EmployeeModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,47 @@ namespace NewsTech.Models
 {
 	public class EmployeeModel
 	{
+		public EmployeeModel() {
+			GenderList = new List<SelectListItem>();
+			AvailableRoles = new List<SelectListItem>();
+			MaritalStatus = new List<SelectListItem>();
+		}
+
+
 		public string Id { get; set; }
+
+		[Display(Name = "Ad")]
 		public string Name { get; set; }
+		[Display(Name = "Soyad")]
 		public string Surname { get; set; }
+		[Display(Name = "E-posta")]
 		public string Email { get; set; }
+		[Display(Name = "Doğum Tarihi")]
 		public DateTime Birthdate { get; set; }
+		[Display(Name = "Telefon Numarası")]
+		public string PhoneNumber { get; set; }
+		[Display(Name = "Rol")]
+		public virtual IEnumerable<SelectListItem> AvailableRoles { get; set; }
+		public int SelectedRoleId { get; set; }
+
+		[Display(Name = "Cinsiyet")]
+		public int SelectedGenderId { get; set; }
+		public IEnumerable<SelectListItem> GenderList { get; set; }
+
+		[Display(Name = "Medeni Durum")]
+		public int SelectedMaritalStatusId { get; set; }
+		public IEnumerable<SelectListItem> MaritalStatus { get; set; }
+
+
+	}
+	public enum GenderType
+	{
+		Erkek = 1,
+		Kadın = 2
+	}
+	public enum MaritalStatus
+	{
+		Evli = 1,
+		Bekar = 2
 	}
 }
